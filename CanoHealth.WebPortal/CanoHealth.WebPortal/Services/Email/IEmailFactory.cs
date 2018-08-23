6094ad4dfda8093980c3ae8abff952e97b713a4d
Harnier Suarez <suarezhar@gmail.com>
2018-08-23 09:14:31 -0400

    Adding Email Factory

---
+++ CanoHealth.WebPortal/CanoHealth.WebPortal/Services/Email/IEmailFactory.cs
@@ -0,0 +1,11 @@
+﻿using CanoHealth.WebPortal.Core.Domain;
+using CanoHealth.WebPortal.Core.Dtos;
+using System.Collections.Generic;
+
+namespace CanoHealth.WebPortal.Services.Email
+{
+    public interface IEmailFactory
+    {
+        Emails GetExpireLicenseTemplate(IEnumerable<NotificationDto> notifications);
+    }
+}
